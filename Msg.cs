7069785f36d2ad90cd8d60cd7d6f1656387d692b
yuansan <11@11>
2022-05-27 12:37:13 +0800

    1111

---
+++ Msg.cs
@@ -0,0 +1,21 @@
+﻿using Hotfix.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotfix.BCBM
+{
+	public class msg_last_random:msg_last_random_base
+	{
+		public string r_;
+		public string t_;
+	}
+
+	public class msg_random_result: msg_random_result_base
+	{
+
+		public string rand_result_;
+	}
+}
